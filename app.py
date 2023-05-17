f3c9bf4d5d8569c6c1e1207aad4a2a75acc808eb
Thomas Charles <19232@wc.school.nz>
2023-05-17 14:34:11 +1200

    v4.4
    -reintegrated admin signup after it was lost due to an error
    - added comments

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+
+# ---------- Imports ----------
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
 
+# ---------- Variables ----------
 DATABASE = "C:/Users/garet/OneDrive/Desktop/Thom/Programming/" \
            "GitHub Projects/April2023-AS91906AS91902-MaoriDictionary/dictionary.db"
 app = Flask(__name__)
@@ -10,6 +13,8 @@ bcrypt = Bcrypt(app)
 app.secret_key = "y4nm1e1w4cz3r3a"
 
 
+# ---------- Functions ----------
+# These functions are called by app functions and contain common task
 def is_logged_in():  # Checks if the user is logged in
     if session.get("email") is None:
         return False
@@ -27,6 +32,10 @@ def create_connection(db_file):  # Creates a connection to the database
     return None
 
 
+# ---------- App Functions ----------
+# All app functions render a template, or reroute to another app route that renders a template
+# All app routes pass the 'logged_in' bool onto the template
+
 @app.route('/')
 def render_home():  # Takes the user to the home page
     return render_template('home.html', logged_in=is_logged_in())
@@ -34,87 +43,111 @@ def render_home():  # Takes the user to the home page
 
 @app.route('/login', methods=['POST', 'GET'])
 def render_login():  # takes the user to the login page
-    if is_logged_in():
+    if is_logged_in():  # if the user is already logged in, redirects home
         return redirect("/")
-    if request.method == 'POST':
+
+    if request.method == 'POST':  # checks if the function is being called in association with a form being submitted
+        # requests the users email and password from the form
         email = request.form['email'].strip().lower()
         password = request.form['password'].strip()
 
+        # establishes query and creates a connection to the database
+        # query collects the primary key, name, hashed password, and admin status \
+        # associated with a variable email \
+        # from the users table
         query = """SELECT PK, Firstname, Hashpass, Admin FROM users WHERE email = ?"""
         con = create_connection(DATABASE)
         cur = con.cursor()
+
+        # collects the user data from the database according to the query using the forms email
         cur.execute(query, (email, ))
         user_data = cur.fetchall()[0]
         con.close()
-        print(user_data)
+
+        # trys to define variables using the data collected from the query
         try:
             user_id = user_data[0]
             first_name = user_data[1]
             hashpass = user_data[2]
             admin = user_data[3]
-        except IndexError:
+        except IndexError:  # if this try detects an IndexError
+            # reroutes back to the login page and returns an error
             print("dataError")
             return redirect("/login?error=Invalid+username+or+password")
+
+        # checks if the password entered fails to match the hashed password logged
         if not bcrypt.check_password_hash(hashpass, password):
+            # reroutes back to the login page and returns an error
             print("passwordError")
             return redirect(request.referrer + "?error=Invalid+username+or+password")
 
+        # if all checks are passed, creates a session using the users details
         session['email'] = email
         session['userid'] = user_id
         session['firstname'] = first_name
         session['admin'] = admin
-        print(session)
-        return redirect('/')
+        return redirect('/')  # redirects home
     return render_template('login.html', logged_in=is_logged_in())
 
 
 @app.route('/logout')
-def logout():
-    print(list(session.keys()))
-    [session.pop(key) for key in list(session.keys())]
-    print(list(session.keys()))
-    return redirect('/?message=See+you+next+time!')
+def logout():  # logs the user out
+    [session.pop(key) for key in list(session.keys())]  # removes each session element
+    return redirect('/?message=See+you+next+time!')  # redirects home
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def render_signup():  # takes the user to the signup page and gathers their signup info
-    if is_logged_in():
+    if is_logged_in():  # if the user is already logged in, redirects home
         return redirect("/")
-    if request.method == 'POST':
-        print(request.form)
+    if request.method == 'POST':  # checks if the function is being called in association with a form being submitted
+
+        # puts data from the form into variables
         first_name = request.form.get('Firstname').title().strip()
         last_name = request.form.get('Lastname').title().strip()
         email = request.form.get('email').lower().strip()
         password = request.form.get('password')
         password2 = request.form.get('password2')
+        admin = request.form.get('admin')
 
+        # checks if the second password fails to match the first or if the password is too short
         if password != password2:
-            return redirect("/signup?error=Passwords+do+not+match")
+            return redirect("/signup?error=Passwords+do+not+match")  # redirects home
         if len(password) < 8:
-            return redirect("/signup?error=Password+must+be+at+least+8+characters")
+            return redirect("/signup?error=Password+must+be+at+least+8+characters")  # redirects home
 
+        # hashes the password
         hashed_password = bcrypt.generate_password_hash(password)
+
+        # creates a connection the database and establishes a query
         con = create_connection(DATABASE)
-        query = "INSERT INTO users (Firstname, Lastname, Email, Hashpass) VALUES (?, ?, ?, ?)"
+        query = "INSERT INTO users (Firstname, Lastname, Email, Hashpass, Admin) VALUES (?, ?, ?, ?, ?)"
         cur = con.cursor()
 
+        # trys to execute the query
         try:
-            cur.execute(query, (first_name, last_name, email, hashed_password))
-        except sqlite3.IntegrityError:
+            cur.execute(query, (first_name, last_name, email, hashed_password, admin))
+        except sqlite3.IntegrityError:  # if it fails due to the email being a dupe redirects home
             con.close()
             return redirect("/signup?error=Email+is+already+used")
 
         con.commit()
         con.close()
-        return redirect('/login')
+        return redirect('/login')  # redirects to the login page
 
     return render_template("signup.html", logged_in=is_logged_in())
 
 
 @app.route('/admin')
-def render_admin():
-    if not is_logged_in():
+def render_admin():  # takes the user to the admin page
+    if not is_logged_in():  # if the user is not logged in redirects them to the home page
         return redirect('/')
+
+    # creates a connection to the database
+    # establishes and executes three queries
+    # query one collects primary key, and category from the categories table
+    # query two collects the primary key, and level from the levels table
+    # query two collects the maori, english, and pk from the words table
     query = "SELECT PK, Category " \
             "FROM Categories"
     con = create_connection(DATABASE)
@@ -134,21 +167,27 @@ def render_admin():
     cur.execute(query)
     words = cur.fetchall()
     con.close()
+    # passes on the collected data to the admin template
     return render_template('admin.html', categories=categories, levels=levels, words=words, logged_in=is_logged_in())
 
 
 @app.route('/add/<table>/<column>', methods=['POST', 'GET'])
-def add(table, column):
-    if not is_logged_in():
+def add(table, column):  # adds a level or category
+    if not is_logged_in():  # if the user isn't logged in redirects them home
         return redirect('/?message=Need+to+be+logged+in.')
     if request.method == "POST":
-        print(request.form)
+
+        # assigns the information in the form and session to variables
         name = request.form.get('name').title().strip()
-        print(name)
         user = session.get("userid")
+
+        # establishes a query and creates a connection to the database
+        # query inserts a name, and user into a table
         con = create_connection(DATABASE)
         query = "INSERT INTO " + table + " (" + column + ", User) VALUES (?, ?)"
         cur = con.cursor()
+
+        # executes the query using the form name and current session userid
         cur.execute(query, (name, user))
         con.commit()
         con.close()
@@ -156,36 +195,52 @@ def add(table, column):
 
 
 @app.route('/remove/<table>/', methods=['POST', 'GET'])
-def remove(table):
-    if not is_logged_in():
+def remove(table):  # removes an entry
+    if not is_logged_in():  # checks if the user is logged in and if not returns them to the home page
         return redirect('/?message=Need+to+be+logged+in.')
     if request.method == "POST":
+
+        # checks that the user typed CONFIRM correctly
         if request.form.get('confirm') == "CONFIRM":
-            print(request.form)
+
+            # assigns the pk in the form to a variable
             pk = request.form.get('pk')
-            print(pk)
+
+            # connects to the database and establishes a query
+            # the query deletes an entry from a table
             con = create_connection(DATABASE)
             query = "DELETE FROM " + table + " WHERE PK = ?"
             cur = con.cursor()
+
+            # executes the query in a table using the forms pk
             cur.execute(query, (pk, ))
             con.commit()
             con.close()
+
+            # redirects the user to the admin page
             return redirect('/admin')
         else:
+            # redirects the user to the admin page
             return redirect('/admin?message=Confirmation+failed.')
 
 
 @app.route('/addword', methods=['POST', 'GET'])
-def add_word():
-    if not is_logged_in():
+def add_word():  # adds a word to the words table
+    if not is_logged_in():  # checks if the user is logged in and if not redirects them to the home page
         return redirect('/?message=Need+to+be+logged+in.')
     if request.method == "POST":
+
+        # assigns the data in the form to variables
         maori = request.form.get('maori').title().strip()
         english = request.form.get('english').title().strip()
         definition = request.form.get('definition').title().strip()
         category = request.form.get('category')
         level = request.form.get('level')
         user = session.get("userid")
+
+        # establishes a query and connects to the database
+        # query inserts a maori word, an english word, a category, a definition, a level, and an owner /
+        # into the words table
         con = create_connection(DATABASE)
         query = "INSERT INTO words (Maori, English, Category, Definition, Level, Owner) VALUES (?, ?, ?, ?, ?, ?)"
         cur = con.cursor()
@@ -242,6 +297,9 @@ def render_editer(word):
     cur.execute(query)
     word_data = cur.fetchone()
     con.close()
+
+    word_level = 1
+    word_category = 1
     for level in levels:
         if word_data[3] == level[0]:
             word_level = level
